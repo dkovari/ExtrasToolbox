df567113c4c14f2e2c3764773ba00cc5d9763641
Dan Kovari - SilverScope <dkovari@gmail.com>
2019-01-16 16:43:08 -0500

    implemented automatic object destruction on clear mex
    added code to automatically setup mexAtExit() to clear managed objects upon "clear mex"

--- include/extras/SessionManager/ObjectManager.h
+++ include/extras/SessionManager/ObjectManager.h
@@ -12,18 +12,52 @@ This is intended to be used with Session.m
 #include <unordered_map>
 #include <memory>
 #include <mex.h>
-#include <functional>
+#include <list>
 
 namespace extras{namespace SessionManager{
 
 
+
+	/// ObjectManager base class
+	/// used to setup the static mexAtExit function, calling ObjMan::clearObjects() for all object managers
+	struct ObjectManager_Base {
+		virtual ~ObjectManager_Base();
+		virtual void clearObjects() = 0;
+		ObjectManager_Base();
+	};
+
+	bool g_MexAtExitSet = false; ///< global flag specifying if mexAtExit has been set
+	std::list<ObjectManager_Base*> g_ObjectManagerBaseList; ///< global list of object managers
+
+	/// called at MexAtExit
+	/// clears all objectmanagers
+	void ExitFn() {
+		for (auto pObj : g_ObjectManagerBaseList) {
+			pObj->clearObjects();
+		}
+	}
+
+	ObjectManager_Base::~ObjectManager_Base() {
+		// remove self from om list
+		g_ObjectManagerBaseList.remove(this);
+	}
+
+	ObjectManager_Base::ObjectManager_Base() {
+		// add self to static OM list
+		g_ObjectManagerBaseList.push_back(this);
+
+		if (!g_MexAtExitSet) {
+			//void(*pfn)(void) = ;
+			///mexAtExit(&ObjectManager_Base::ExitFn);
+			g_MexAtExitSet = true;
+		}
+	}
+
 	template <class Obj>
-	class ObjectManager {
+	class ObjectManager: virtual public ObjectManager_Base {
 	protected:
 		bool _lock_mex;
 		std::unordered_map<intptr_t, std::shared_ptr<Obj>> ObjectMap;
-
-		
 
 		static intptr_t getIntPointer(const mxArray* pointer) {
 			if (mxIsEmpty(pointer))
@@ -35,65 +69,21 @@ namespace extras{namespace SessionManager{
 			return *reinterpret_cast<intptr_t*>(mxGetData(pointer));
 		}
 
-		void clearObjects() {
-			for (auto it = ObjectMap.begin(); it != ObjectMap.end();) {
-				mexPrintf("id: %d\n", it->first);
-				mexEvalString("pause(0.2)");
-				destroy(it->first);
-			}
-		}
-
 	public:
-		static std::list < ObjectManager<Obj>*> OM_list;
-		static bool exitSet;
 
-		static void exitFn() {
+		/// destruct all managed objects
+		void clearObjects() {
 #ifdef _DEBUG
-			mexPrintf("in exitFn()\n");
-			mexPrintf("Clearing mexFunction: %s\n", mexFunctionName());
-			mexEvalString("pause(0.2)");
+			mexPrintf("ObjectManager<%s>::clearObjects()\n", typeid(Obj).name());
 #endif
-			for (auto pOB : OM_list) {
-				pOB->clearObjects();
-			}
+			ObjectMap.clear();
 		}
-
 
 		ObjectManager(bool LOCK_MEX = true){
 			_lock_mex = LOCK_MEX;
 #ifdef _DEBUG
 			mexPrintf("Creating ObjectManager<%s>\n", typeid(Obj).name());
-			if (_lock_mex) {
-				mexPrintf("\tLocking mex file: %s\n", mexFunctionName());
-				mexEvalString("pause(0.2)");
-			}
 #endif
-			if (!_lock_mex) {
-#ifdef _DEBUG
-				mexPrintf("registering with OM_list\n");
-				mexEvalString("pause(0.2)");
-#endif
-#ifdef _DEBUG
-				mexPrintf("OM_list.size(): %d\n", OM_list.size());
-				mexEvalString("pause(0.2)");
-#endif
-				// register with static OM_list
-				OM_list.push_back(this);
-
-#ifdef _DEBUG
-				mexPrintf("OM_list.size(): %d\n",OM_list.size());
-				mexEvalString("pause(0.2)");
-#endif
-
-				if (!exitSet) {
-#ifdef _DEBUG
-					mexPrintf("set mexAtExit(...)\n");
-					mexEvalString("pause(0.2)");
-#endif
-					mexAtExit(&ObjectManager::exitFn);
-					exitSet = true;
-				}
-			}
 		}
 
 		~ObjectManager(){
@@ -102,20 +92,10 @@ namespace extras{namespace SessionManager{
 			mexEvalString("pause(0.2)");
 #endif
 			clearObjects();
-
-			for (auto it = OM_list.begin(); it != OM_list.end();) { //deregister from list
-				if (*it == this) {
-					OM_list.erase(it);
-				}
-				else {
-					++it;
-				}
-			}
-
 		}
 
-		//Add object to map, creates a shared_ptr from the pointer
-		// call using something like objman.create(new YourObj());
+		///Add object to map, creates a shared_ptr from the pointer
+		/// call using something like objman.create(new YourObj());
 		intptr_t create(Obj* p) { 
 #ifdef _DEBUG
 			mexPrintf("ObjectManager<%s>::create\n", typeid(Obj).name());
@@ -129,14 +109,13 @@ namespace extras{namespace SessionManager{
 				mexLock(); //increment mex lock counter;
 			}
 			
-
 			return ptr;
 		}
 
-		//destroy instance
+		/// destroy instance
 		void destroy(intptr_t id) {
 #ifdef _DEBUG
-			mexPrintf("ObjectManager<%s>::destroy(intptr_t id)\n", typeid(Obj).name());
+			mexPrintf("ObjectManager<%s>::destroy(%d)\n", typeid(Obj).name(),id);
 			mexEvalString("pause(0.2)");
 #endif
 			if (!ObjectMap.empty())
@@ -147,13 +126,15 @@ namespace extras{namespace SessionManager{
 				}
 			}
 		}
+
+		/// destroy instance specified in mxArray
 		void destroy(const mxArray* in) {
 			intptr_t id = getIntPointer(in);
 			destroy(id);
 		}
 
 		std::shared_ptr<Obj> get(intptr_t id) {
-#ifdef _DEBUG
+#ifdef DAN_DEBUG
 			mexPrintf("ObjectManager<%s>::get()\n\tnObjects=%d\n", typeid(Obj).name(),ObjectMap.size());
 			mexEvalString("pause(0.2)");
 #endif
@@ -162,13 +143,16 @@ namespace extras{namespace SessionManager{
 				return search->second;
 			}
 			else {
-				throw(std::runtime_error("ObjectManager::get() -> Object not found"));
+				throw(std::runtime_error(
+					std::string("ObjectManager::get(")+
+					std::to_string(id)+
+					std::string(") -> Object not found")));
 			}
 		}
 
 		std::shared_ptr<Obj> get(const mxArray* in) {
 			intptr_t id = getIntPointer(in);
-#ifdef _DEBUG
+#ifdef DAN_DEBUG
 			mexPrintf("\t ptr=%d\n", typeid(Obj).name(),id);
 			mexEvalString("pause(0.2)");
 #endif
@@ -176,5 +160,3 @@ namespace extras{namespace SessionManager{
 		}
 	};
 }}
-template <class Obj> std::list < extras::SessionManager::ObjectManager<Obj>*> extras::SessionManager::ObjectManager<Obj>::OM_list = std::list < extras::SessionManager::ObjectManager<Obj>*>();
-template <class Obj> bool extras::SessionManager::ObjectManager<Obj>::exitSet = false;
--- msvc/TestCode/TestCode.cpp
+++ msvc/TestCode/TestCode.cpp
@@ -8,70 +8,33 @@
 #include <string>
 
 
-template<typename T=double>
-struct A {
-	int one;
-	T two;
+class A {
+public:
+	static bool sV;
 
-	A(int a, T b) :one(a), two(b) {};
+	A() {
+		std::cout << "sV: " << sV << std::endl;
+		sV = false;
+	}
 };
-struct B: public A<> {
+bool A::sV = true;
 
-	B(int x, int y) :A(x,y) {};
-};
-
-
-template<typename T=double>
-struct C {
-	virtual void sz() const = 0;
-};
-template<> void C<double>::sz() const {
-	using namespace std;
-	cout << "C<double>::sz()" << endl;
-}
-
-struct D :public C<int> {
-	void sz() const {
-		using namespace std;
-		cout << "D::sz()->C<int>" << endl;
+template<typename T>
+class B : public A {
+public:
+	T val = 0;
+	B() {
+		std::cout << "B::Type: " << typeid(val).name() << std::endl;
 	}
 };
 
 
-struct D2 :public C<> {
-	void sz() const {
-		C<>::sz();
-	}
-};
-
-
-typedef std::map<std::string, int> MapT;
 int main()
 {
 	using namespace std;
 
-	MapT myMap;
-	myMap.insert(MapT::value_type("A",1));
-	myMap.insert(MapT::value_type("B", 1));
-
-	auto it = myMap.find("A");
-
-
-
-	B myB(1, 2);
-	cout << "B.one: " << myB.one << endl;
-	cout << "B.two: " << myB.two << endl;
-	cout << "type:" << typeid(myB.two).name() << endl;
-
-	cout << "size_t(false)=" << size_t(false) << " size_t(true)=" << size_t(true) << endl;
-
-	cout << "Try D" << endl;
-	D d;
-	d.sz();
-
-	cout << "Try D2" << endl;
-	D2 c;
-	c.sz();
+	B<int> one;
+	B<double> two;
 
 
     return 0;
