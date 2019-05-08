fa47ba8db054eb06347693679beeed2102e97533
Dan Kovari - SilverScope <dkovari@gmail.com>
2019-05-08 15:24:00 -0400

    Update expandAxes.m

--- +extras/expandAxes.m
+++ +extras/expandAxes.m
@@ -1,5 +1,10 @@
 function expandAxes(hax,axis_opts)
 % expand axes to fill figure
+%
+% hAx: axes handle to expand
+% axis_opts: (optional) char array specifying extra axis option to apply
+%   e.g. axis_opts = 'image' to force square pixel scale format
+% you can specify multiple axis options by passing a cellstr array
 
 if nargin<1
     hax = gca;
@@ -10,7 +15,14 @@ assert(isgraphics(hax)&&strcmpi(hax.Type,'axes'),'expandAxes only works on axes 
 
 axis(hax,'tight');
 if nargin>1
-    axis(hax,axis_opts);
+    asert(ischar(axis_opts)||iscellstr(axis_opts),'axis_opts must be char or cellstr');
+    if iscellstr(axis_opts)
+        for n=1:numel(axis_opts)
+            axis(hax,axis_opts{n});
+        end
+    else
+        axis(hax,axis_opts);
+    end
 end
 %axis(hax,'image');
 try
